6f60485681863ed0782c5c9a09f6eb787302b4ea
ralvarezlo <ralvarezlo@miners.utep.edu>
2021-01-30 22:09:09 -0700

    fixed Bit0 on p1out logic

--- msp430-check/p2sw-demo.c
+++ msp430-check/p2sw-demo.c
@@ -1,3 +1,4 @@
+
 /** brief program based on the button demo
  *  will play a different sound and turn on a LED w/ each button
  */
@@ -12,19 +13,23 @@ void button_machine()
 {
     u_int switches = p2sw_read(), i;
     char str[5];
+    char ledFlag = 0;
+
+    P1OUT &= 0xBE;
+    switch(switches & 0x0F) {
+    case 14: buzzer_set_period(500); break;
+    case 13: buzzer_set_period(750); break;
+    case 11: buzzer_set_period(1000); break;
+    case 7: buzzer_set_period(1200); break;
+    default: buzzer_set_period(0); ledFlag = 1;
+    }
     
     for (i = 0; i < 4; i++)
       str[i] = (switches & (1<<i)) ? '-' : '0'+i;
     str[4] = 0;
     drawString5x7(20,20, str, COLOR_GREEN, COLOR_BLUE);
 
-    switch(switches & 0x0F) {
-    case 14: buzzer_set_period(500); break;
-    case 13: buzzer_set_period(750); break;
-    case 11: buzzer_set_period(1000); break;
-    case 7: buzzer_set_period(1200); break;
-    default: buzzer_set_period(0);
-    }
+    P1OUT |= ledFlag? 0x40 : 0x01;
 }
 
 
@@ -37,7 +42,6 @@ void main()
   buzzer_init();
   buzzer_set_period(0);
   P1DIR |= 0x41;
-  P1OUT |= 0x40; // ERROR WHEN TURNING ON BIT0
   or_sr(0x8);			/* GIE (enable interrupts) */
   u_char width = screenWidth, height = screenHeight;
 
